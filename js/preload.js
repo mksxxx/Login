57d2bea0bd74a64169b210c73e637c048631e84f
Marcos Vinicius <marcos.10823460@gmail.com>
2025-10-20 21:30:47 -0300

    Consumindo os dados do cadastro do usuario, usuario logado e logout

--- js/preload.js
+++ js/preload.js
@@ -4,5 +4,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
     login: (email, password) => ipcRenderer.invoke('login', email, password),
     abrirHome: () => ipcRenderer.send('abrir-home'),
-    cadastrarUsuario: (dados) => ipcRenderer.invoke('cadastrar-usuario', dados)
+    cadastrarUsuario: (dados) => ipcRenderer.invoke('cadastrar-usuario', dados),
+    getUsuarioLogado: () => ipcRenderer.invoke('get-usuario-logado'),
+    logout: () => ipcRenderer.send('logout')
 });
